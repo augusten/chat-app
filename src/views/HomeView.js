d4edb0c102fe52d2ed77e786834245b907a8c30b
Augustė Nausėdaitė <auguste.nausedaite@gmail.com>
2017-01-28 13:41:00 +0100

    initial version of chat is kinda working

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,6 +1,9 @@
 import Surface               from 'famous/core/Surface.js';
+import Engine               from 'famous/core/Engine.js';
 import InputSurface          from 'famous/surfaces/InputSurface.js';
-import SubmitInputSurface    from 'famous/surfaces/SubmitInputSurface.js';
+import TextareaSurface       from 'famous/surfaces/TextareaSurface.js';
+import GridLayout            from 'famous/views/GridLayout.js';
+// import SubmitInputSurface    from 'famous/surfaces/SubmitInputSurface.js';
 import {View}                from 'arva-js/core/View.js';
 import {layout, event}       from 'arva-js/layout/Decorators.js';
 import {SingleLineTextInput} from 'arva-js/components/inputs/SingleLineTextInput.js';
@@ -10,6 +13,15 @@ import CollectionLayout      from 'famous-flex/layouts/CollectionLayout.js';
 import {Note, Notes}         from '../models/HomeModel.js'
 import Utility               from 'famous/utilities/Utility.js'
 
+
+// export class ViewWithGrid extends View {
+// 	mainContext = Engine.createContext();
+// 	surfaces = []
+
+// 	grid = new GridLayout({
+// 		dimensions: [1, 3]
+// 	}).sequenceFrom( surfaces )
+// }
 
 export class ViewWithTop extends View {
 	// create greeting
@@ -43,16 +55,17 @@ export class ViewWithScroll extends ViewWithTop {
 	// creates the scroll view of messages
     @layout.size(600, 150)
     @layout.stick.center()
+    @layout.translate(0, -100, 0)
 
     scrollView = new DataBoundScrollView({
     	mouseMove: true,
     	scrollToNewChild: true,
 	    layout: CollectionLayout,
 	    layoutOptions: {
-	        itemSize: [undefined, 30]
+	        itemSize: [undefined, undefined]
 	    },
 	    itemTemplate: note => 
-	    	new Surface({ content: `Message: ${note.text}`}),
+	    	new Surface({ content: `${note.text}`}),
 	    dataStore: new Notes(),
 		chatScrolling: true
     })
@@ -65,10 +78,18 @@ export class HomeView extends ViewWithScroll {
 
 	// creates the input fields
 
+	// author input
+	/* The size of this surface is 300x25 px */
+	@layout.stick.bottom()
+	@layout.translate(0, -80, 0)
+    @layout.size(500, 35)
+
+    inputName = new SingleLineTextInput( { placeholder: "name" } )
+
 	/* The size of this surface is 300x25 px */
 	@layout.dock.bottom()
-    @layout.size(500, 35)
-    /* Place it in the midddle of the view */
+	@layout.translate(0, -5, 0)
+    @layout.size(500, 70)
     @layout.stick.center()
     // @layout.animate({
     // 	waitFor: 'scrollView',
@@ -82,5 +103,9 @@ export class HomeView extends ViewWithScroll {
     		this.options.sendMessage( e.target.value )
     	}
     })
-    input = new SingleLineTextInput( { placeholder: "typetypetype" } )
+    input = new TextareaSurface( { 
+    	placeholder: "message",
+    	wrap: "hard",
+    	size: true
+    	 } )
 }
